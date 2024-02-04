755f91eb7623fe5096a6c60635ba931ad850edf9
mazzillio <mateca500@gmail.com>
2024-02-04 12:34:29 -0300

    create logic of jwttoken

--- src/main/java/com/mazzillio/med/voll/api/controller/AuthenticationController.java
+++ src/main/java/com/mazzillio/med/voll/api/controller/AuthenticationController.java
@@ -1,6 +1,9 @@
 package com.mazzillio.med.voll.api.controller;
 
 import com.mazzillio.med.voll.api.domain.user.AuthenticateData;
+import com.mazzillio.med.voll.api.domain.user.User;
+import com.mazzillio.med.voll.api.infra.security.JWTTokenData;
+import com.mazzillio.med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -17,10 +20,14 @@ public class AuthenticationController {
 
     @Autowired
     private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenService tokenService;
     @PostMapping
     public ResponseEntity<Object> login(@RequestBody AuthenticateData data){
-        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(data.login(),data.password());
-        Authentication auth = authenticationManager.authenticate(token);
-        return ResponseEntity.ok().build();
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(data.login(),data.password());
+        Authentication auth = authenticationManager.authenticate(authenticationToken);
+        String tokenJWT = tokenService.generateToken((User) auth.getPrincipal());
+        return ResponseEntity.ok(new JWTTokenData(tokenJWT));
     }
 }
---
+++ src/main/java/com/mazzillio/med/voll/api/infra/security/JWTTokenData.java
@@ -0,0 +1,4 @@
+package com.mazzillio.med.voll.api.infra.security;
+
+public record JWTTokenData(String token) {
+}
